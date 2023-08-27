8d2609f3a6fff24870a9f1339ca126f76b113c27
Paweł Lenartowicz <6quarg@gmail.com>
2023-08-27 12:24:20 +0200

    readed control to MixedGen

--- creating_training_datasets_2.py
+++ creating_training_datasets_2.py
@@ -32,7 +32,12 @@ def MixedGenerator(numberOfJunkQuestionaries = 1, customParam = 1, junkRatio = (
     x2 =   gen.corelated_junk_group(customParam, junkRatio, maxDispersion, noise1)
     x3 = gen.uncorelated_junk_group(customParam, junkRatio, maxDispersion)
     x4 =       gen.equal_junk_group(customParam, junkRatio, maxDispersion, noise2)
-    datasets = x1+x2+x3+x4
+    if bootstrap == False:
+        x_control = generator(numberOfJunkQuestionaries*4, returnParametres, truePerJunkRatio, minSize, maxSize, bootstrap, padding).control_group(customParam)
+        datasets = x1+x2+x3+x4+x_control
+    else:
+        datasets = x1+x2+x3+x4
+        
     shuffle(datasets)
 
     return(datasets)
